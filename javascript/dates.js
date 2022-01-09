5bea25dc31c9692d46fe8158b430a0ba43fdf8a2
Irythom <ryanweinheimer@gmail.com>
2022-01-08 23:12:00 -0700

    Finished up the main page

--- javascript/dates.js
+++ javascript/dates.js
@@ -1,2 +1,2 @@
-document.querySelector('#lastmod').innerHTML = `Date the <strong>document</strong> was last modified: ${document.lastModified}`;
+document.querySelector('#lastmod').innerHTML = `Date this <strong>page</strong> was last modified: ${document.lastModified}`;
 document.getElementById("year").innerHTML = new Date().getFullYear();
