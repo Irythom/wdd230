fabc9498e2048ab8a7ee0908b947242dba99eb7d
Irythom <ryanweinheimer@gmail.com>
2022-01-21 19:01:57 -0700

    Attempt #1 to fix error for week 2 assignment

---
+++ lesson2/lesson2-js/2-dates.js
@@ -0,0 +1,2 @@
+document.querySelector('#lastmod').innerHTML = `Date this <strong>page</strong> was last modified: ${document.lastModified}`;
+document.getElementById("year").innerHTML = new Date().getFullYear();
