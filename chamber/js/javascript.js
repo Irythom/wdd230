ec83215f143e1220e88f4a589b48aeb6c59a86ba
Irythom <ryanweinheimer@gmail.com>
2022-01-28 02:53:19 -0700

    Finished week 4 assignment

--- chamber/js/javascript.js
+++ chamber/js/javascript.js
@@ -1,3 +1,12 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+document.querySelector('#lastmod').innerHTML = `${document.lastModified}`;
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+document.getElementById("header-footer-date").innerHTML = fulldate
