7bbf6b173c24b33af74342dd167ea629cfbc7a52
Irythom <ryanweinheimer@gmail.com>
2022-02-05 13:23:18 -0700

    Week 5 changes

--- chamber/js/javascript.js
+++ chamber/js/javascript.js
@@ -10,3 +10,32 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
 document.getElementById("header-footer-date").innerHTML = fulldate
+
+let banner;
+const currentDay = new Date().getDay();
+
+switch(currentDay) {
+  case 0:
+    banner = ".bannertext {display: none;}";
+    break;
+ case 1:
+    banner = ".bannertext {display: block;}";
+    break;
+ case 2:
+    banner = ".bannertext {display: block;}";
+    break;
+ case 3:
+    banner = ".bannertext {display: none;}";
+    break;
+ case 4:
+   banner = ".bannertext {display: none;}";
+    break;
+ case 5:
+    banner = ".bannertext {display: none;}";
+    break;
+ case 6:
+    banner = ".bannertext {display: none;}";
+    break;
+}
+
+document.getElementById("bannertextstyle").innerHTML = banner;
---
+++ lesson5/js/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector(".list");
+
+
+button.addEventListener('click', function() {
+  let myItem = input.value;
+  input.value = '';
+
+  const listItem = document.createElement('li');
+  const listText = document.createElement('span');
+  const listBtn = document.createElement('button');
+
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = '❌';
+  list.appendChild(listItem);
+
+  listBtn.onclick = function(e) {
+  list.removeChild(listItem);
+  }
+
+  input.focus();
+}
+    
+);
