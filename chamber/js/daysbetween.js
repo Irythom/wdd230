e5545cd6fecb3c1f424b2a80632f804241d6893f
Irythom <ryanweinheimer@gmail.com>
2022-02-19 21:18:09 -0700

    fixed the js

--- chamber/js/daysbetween.js
+++ chamber/js/daysbetween.js
@@ -2,16 +2,18 @@ const lastVisitDisplay = document.querySelector("#visits");
 
 const lastvisit = localStorage.getItem("lastvisit");
 
-if (lastvisit !== 0) {
-	lastVisitDisplay.textContent = daysbetween;
-} else {
-	lastVisitDisplay.textContent = `This is your first visit!`;
-}
-
 const factor = 1000 * 60 * 60 * 24;
 
-let daysbetween = Date.now() - lastvisit;
+const daysbetween = Date.now() - lastvisit;
 
-let numberOfDays = daysbetween / factor;
+const numberOfDays = daysbetween / factor;
+const wholeDays = Math.round(numberOfDays)
 
-localStorage.setItem("lastvisit", Date.now());
+if (!lastvisit) lastVisitDisplay.textContent = `This is your first visit!`;
+if (lastvisit && wholeDays === 0) {
+	lastVisitDisplay.textContent = 'You have already visited today';
+} else {
+	lastVisitDisplay.textContent = wholeDays;
+}
+
+localStorage.setItem("lastvisit", Date.now() - 983423324888 );
